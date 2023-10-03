0953008a733e4d81029f02f7f9d2cabb594e562f
Бабийчук Богдан <babiychuk.bogdan@mail.ru>
2023-10-03 07:57:18 +0200

    flake8v8

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 
-
 class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         """
@@ -8,10 +7,8 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
         для всех пользователей, а также для аутентифицированных пользователей
         для всех остальных методов.
         """
-        return (
-            request.method in permissions.SAFE_METHODS or
-            request.user.is_authenticated
-        )
+        return (request.method in permissions.SAFE_METHODS or
+                request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         """
